6ffd1dcba1bca7367a60d70c02880b8efb3f2a74
Moonlander09 <webdev095@gmail.com>
2025-04-03 20:00:11 +0530

    minor error fixes

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./db.js";
-import router from './Routes/blogRoute.js'
-dotenv.config({path:'./.env'}); // Load environment variables
+import router from "./Routes/blogRoute.js";
+dotenv.config({ path: "./.env" }); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -11,17 +11,16 @@ connectDB();
 
 // Middleware
 // Enable CORS with credentials support
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     credentials: true,
-}));
-  
-  
+  })
+);
+
+
 app.use(express.json()); // Allow JSON data in requests
 
-
-app.use('/api/blog',router)
+app.use("/api/blog", router);
 // Start the server
-app.listen(PORT)
-
-
+app.listen(PORT);
